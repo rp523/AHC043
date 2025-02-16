3e4c9bb066ad219ad18768ead73446a7c4c27f2f
Yusuke Watanabe <isgsktyktt@gmail.com>
2025-02-16 20:49:18 +0900

    eval

--- scan.py
+++ scan.py
@@ -1,11 +1,20 @@
 from subprocess import getoutput
 import time
-cmd = "cargo fmt && cargo build -r"
+DEBUG = False
+
+print("DEBUG: ", DEBUG)
+cmd = "cargo fmt && cargo build"
+if not DEBUG:
+    cmd += " -r"
 ret = getoutput(cmd)
 dt_max = 0
 for i in range(100):
     print(i, end = ":")
-    cmd = "cargo run -r < tools/in/{0:04d}.txt > tools/out/{0:04d}.txt".format(i, i)
+    cmd = "cargo run"
+    if not DEBUG:
+        cmd += " -r"
+    cmd += " < tools/in/{0:04d}.txt > tools/out/{0:04d}.txt".format(i, i)
+    print(cmd, end = " ")
     t0 = time.time()
     ret = getoutput(cmd)
     score = int(ret.split()[-1])
